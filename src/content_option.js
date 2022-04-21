a9ec16108efe0ea32d229c6dd53a4f190bcfca42
vipulkaushik92 <vipulkaushik92@gmail.com>
2022-04-21 19:54:34 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,179 @@
+const logotext = "VIPUL";
+const meta = {
+  title: "Vipul Kaushik",
+  description:
+    "I’m Vipul kaushik mobile application developer,currently working in Malaysian Company",
+};
+const introdata = {
+  title: "I’m Vipul kaushik",
+  animated: {
+    first: "I love coding",
+    second: "I code cool apps",
+    third: "I develop mobile apps",
+  },
+  description:
+    "Hey, I am a mobile application developer focused on the latest and greatest technologies such as React Native to build seamless cross-platform mobile apps.",
+  your_img_url:
+    "https://media-exp1.licdn.com/dms/image/C4E03AQEIPtysPk3atQ/profile-displayphoto-shrink_800_800/0/1591783936389?e=1655942400&v=beta&t=KfIUGETeOVwNCyDL90P6ZqOHriYJUeLswDOM7arvQw0",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "I’m a software engineer at segwitz Sdn Bhd working on react native as a project lead. I enjoy coding and managing people.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Intern (React Native)",
+    where:
+      "Addval Labs - A-621, Bestech Business Towers, Sector 66, Sahibzada Ajit Singh Nagar, Punjab 160059",
+    date: "1st January 2018 - 16th July 2018",
+  },
+  {
+    jobtitle: "React Native Developer",
+    where:
+      "Addval Labs - A-621, Bestech Business Towers, Sector 66, Sahibzada Ajit Singh Nagar, Punjab 160059",
+    date: "16th July 2018 - 1st October, 2020.",
+  },
+  {
+    jobtitle: "React Native Developer (Project Lead)",
+    where:
+      "SegWitz Sdn Bhd (Malaysia) - No.2, Jalan Hang Tuah 6/2, Taman Muhibbah, 86000 Kluang, Johor, Malaysia",
+    date: "9th November 2020 - Present",
+  },
+];
+
+const skills = [
+  {
+    name: "React Native",
+    value: 90,
+  },
+  {
+    name: "Javascript",
+    value: 60,
+  },
+  {
+    name: "Redux",
+    value: 70,
+  },
+  {
+    name: "React JS",
+    value: 60,
+  },
+];
+
+const services = [
+  {
+    title: "Custom Animation",
+    description:
+      "From creating custom anomations with the help of react native reanimated 2, i can build all sorts of animations.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "From building a normal functionaly app to properly handling UI/UX for every screen.",
+  },
+  {
+    title: "Libraries",
+    description:
+      "From integrating thirdparty login sdks like (fb, google, apple), push notifications, socket.io etc.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://segwitz.com/wp-content/uploads/2021/09/cemas-khidmat.png",
+    desctiption:
+      "Cemas Khidmat is an app where car owners can request for towing, mechanic to change tyre, to buy fuel or to troubleshoot issues with their cars.",
+    link: "#",
+  },
+  {
+    img: "https://play-lh.googleusercontent.com/lT7PN2PVVo9dCffZVVNhoeENAFLzGfV_iI5UXnCYa-2P3kxo33cM5EdJhevDjCeoPf4=s180-rw",
+    desctiption:
+      "Cemas Khidmat is an app where car owners can request for towing, mechanic to change tyre, to buy fuel or to troubleshoot issues with their cars.",
+    link: "https://play.google.com/store/apps/details?id=com.ckprovider",
+  },
+  {
+    img: "https://segwitz.com/wp-content/uploads/2021/09/merchant-app-development.png",
+    desctiption:
+      "Loyalbyte will be the loyalty rewards app you ever need on your smartphone. The reward bytes earned are non-expiry, transferable and exchangeable",
+    link: "https://play.google.com/store/apps/details?id=com.loyalbyteuser",
+  },
+  {
+    img: "https://play-lh.googleusercontent.com/3rRhuIhjXho7P1Sp_Y-rMrefvC63ooRHv7Tpp_Nu4k6OZ_Yn2ajAtQU_iDX7RZne1Q=s180-rw",
+    desctiption:
+      "Loyalbyte Merchant app provides full flexibility and versatility for merchants to unleash the power of loyalty marketing.",
+    link: "https://play.google.com/store/apps/details?id=com.loyalbytemerchant",
+  },
+  {
+    img: "https://segwitz.com/wp-content/uploads/2020/02/afa-vendor-app-icon.png",
+    desctiption:
+      "AFA Sports Solution is a startup offering management app for sports complexes to increase their efficiency while also allowing users to  make bookings for sports facilities from their user app..",
+    link: "https://apps.apple.com/my/app/afa-sports-venue-booking-app/id1526420132",
+  },
+  {
+    img: "https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/8a/f2/ec/8af2ece3-d99a-8ea0-97e6-0b5c94d12765/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/230x0w.webp",
+    desctiption:
+      "AFA Sports Solution is a startup offering management app for sports complexes to increase their efficiency while also allowing users to  make bookings for sports facilities from their user app.",
+    link: "https://apps.apple.com/my/app/afa-biz-sports-venue-manager/id1526421908",
+  },
+
+  {
+    img: "https://is4-ssl.mzstatic.com/image/thumb/Purple112/v4/aa/ba/05/aaba0517-035c-b741-ab60-a294bd1bb4e5/AppIcon-0-0-1x_U007emarketing-0-0-0-5-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/230x0w.webp",
+    desctiption:
+      "Teman Malaysia provide care companionship services to improve your loved one’s quality of life and to provide assistance for them.",
+    link: "https://apps.apple.com/in/app/teman/id1575529818",
+  },
+  {
+    img: "https://is4-ssl.mzstatic.com/image/thumb/Purple116/v4/9f/ed/1c/9fed1c0f-0b04-4db4-58bb-854e73da1c79/AppIcon-0-0-1x_U007emarketing-0-0-0-5-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/230x0w.webp",
+    desctiption:
+      "Teman Malaysia provide care companionship services to improve your loved one’s quality of life and to provide assistance for them.",
+    link: "https://apps.apple.com/in/app/temanion-freelancers-app/id1601233926",
+  },
+  {
+    img: "https://is2-ssl.mzstatic.com/image/thumb/Purple116/v4/66/dd/f8/66ddf846-3145-19d2-3080-efa1a4e4760d/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/230x0w.webp",
+    desctiption:
+      "kulcare is a fast & simple to use clinic management app for doctors & clinics. We offer everything you need to grow your practice & engage with your patients.",
+    link: "https://apps.apple.com/in/app/kulcare-for-doctors/id1256481906",
+  },
+  {
+    img: "https://is2-ssl.mzstatic.com/image/thumb/Purple116/v4/66/dd/f8/66ddf846-3145-19d2-3080-efa1a4e4760d/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/230x0w.webp",
+    desctiption:
+      "kulcare is your personal Healthcare manager that helps take care of your well-being like a pro. With the kulcare app you can: - Book a video call",
+    link: "https://apps.apple.com/in/app/kulcare/id1253938993",
+  },
+  {
+    img: "https://is4-ssl.mzstatic.com/image/thumb/Purple113/v4/b5/2b/46/b52b46fb-14e1-fe46-0499-d99b5527a607/AppIcon-0-1x_U007emarketing-0-0-85-220-0-5.png/230x0w.webp",
+    desctiption:
+      "kulcare Pharmacy is a tool for Pharmacies to create their online presence and deliver medicines..",
+    link: "https://apps.apple.com/in/app/kulcare-pharmacy/id1437001441",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "vipulkaushik96@gmail.com",
+  YOUR_FONE: "(+91) 7696763111",
+  description:
+    "Hey, I am a mobile application developer focused on the latest and greatest technologies such as React Native to build seamless cross-platform mobile apps.",
+  YOUR_SERVICE_ID: "service_jhxueap",
+  YOUR_TEMPLATE_ID: "template_8ifuybl",
+  YOUR_USER_ID: "to6jU0nyXb9scdzWQ",
+};
+
+const socialprofils = {
+  github: "https://github.com/vipulkazz",
+  linkedin: "https://www.linkedin.com/in/vipul-kaushik/",
+  twitter: "https://twitter.com/vipulka67375272",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
